32a5b9e2502fae9af839558a10403529f13ef34b
Mick Cooney <mickcooney@gmail.com>
2013-10-16 14:05:52 +0100

    Fixed typos surrounding the renaming of the various time series

--- tsworkshop2013/wsexercise.1.2.R
+++ tsworkshop2013/wsexercise.1.2.R
@@ -6,15 +6,15 @@
 source('setup_data.R', echo = TRUE);
 
 
-class(Maine.month.ts);
-str(Maine.month.ts);
+class(MA.month.ts);
+str(MA.month.ts);
 
-start(Maine.month.ts); end(Maine.month.ts); frequency(Maine.month.ts);
+start(MA.month.ts); end(MA.month.ts); frequency(MA.month.ts);
 
-plot(Maine.month.ts, ylab = "Unemployment data for the state of Maine");
+plot(MA.month.ts, ylab = "Unemployment data for the state of Maine");
 
 
 ### Using ggplot2 looks better, but you have to work hard for the
 ### labels on the x-axis so I am leaving this out for now.
 
-#qplot(1:length(Maine.month.ts), as.vector(Maine.month.ts), geom = 'line', ylab = 'Unemployment data for the state of Maine');
+#qplot(1:length(MA.month.ts), as.vector(MA.month.ts), geom = 'line', ylab = 'Unemployment data for the state of Maine');
