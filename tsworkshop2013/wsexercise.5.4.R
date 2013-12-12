fe88808de13f43f3b833c7d718e7ef13acdb8c61
Mick Cooney <mickcooney@gmail.com>
2013-12-12 20:21:27 +0000

    Added in the .Rhistory entries from the third workshop

--- tsworkshop2013/wsexercise.5.4.R
+++ tsworkshop2013/wsexercise.5.4.R
@@ -6,10 +6,7 @@ source('setup_data.R', echo = TRUE);
 
 
 Complaints.ts <- ts(motororg.df$complaints, start = c(1996, 1), freq = 12)
-
 plot(Complaints.ts, xlab = "Time / months", ylab = "Complaints");
 
-
 Complaints.ts.decomp <- decompose(Complaints.ts);
-
 plot(Complaints.ts.decomp);
--- tsworkshop2013/wsexercise.6.1.R
+++ tsworkshop2013/wsexercise.6.1.R
@@ -8,9 +8,9 @@ set.seed(42);
 
 x <- rnorm(99, 0, 1);
 
-whitenoise.ts     <- ts(c(1, x));
+whitenoise.ts     <- ts(cumsum(c(1, x)));
 whitenoise.ts.acf <- acf(whitenoise.ts, plot = FALSE);
 
 layout(1:2);
-plot(whitenoise.ts,     ylim = c(-4, 4));
-plot(whitenoise.ts.acf, ylim = c(-1, 1));
+plot(whitenoise.ts,     ylim = c(-10, 10));
+plot(whitenoise.ts.acf, ylim = c( -1, 1));
