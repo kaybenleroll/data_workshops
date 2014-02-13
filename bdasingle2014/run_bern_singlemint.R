1809b8f3e559025fc4f1e1ed95e59537be9cd5ee
Mick Cooney <mickcooney@gmail.com>
2014-02-13 15:01:41 +0000

    Split the basic JAGS model into two versions - one for beta, the other for bernouilli

---
+++ bdasingle2014/run_bern_singlemint.R
@@ -0,0 +1,41 @@
+library(rjags);
+library(data.table);
+library(ggplot2);
+
+
+### Set a seed to ensure that the random processes are repeatable.
+set.seed(42);
+
+sample.count <- 1000;
+
+chain.count  <- 3;
+adapt.steps  <- 500;
+burnin.steps <- 1000;
+
+jags.file <- 'singlemint_bern_full.jag';
+
+
+
+generate.jags.bernouilli.samples <- function(mintdata.dt) {
+    data.nTrialTotal <- dim(mintdata.dt)[1];
+    data.nCoins      <- length(unique(mintdata.dt$coinid));
+    data.coin        <- match(mintdata.dt$coinid, unique(mintdata.dt$coinid));
+    data.y           <- mintdata.dt$outcome;
+
+
+    jagsModel <- jags.model(jags.file, data = list(nTrialTotal = data.nTrialTotal, nCoins = data.nCoins, coin = data.coin, y = data.y), n.chains = chain.count, n.adapt = adapt.steps);
+
+    update(jagsModel, n.iter = burnin.steps);
+
+    coda.data <- coda.samples(jagsModel, variable.names = c('mu', 'kappa'), n.iter = sample.count);
+
+    return(coda.data);
+}
+
+
+
+
+### Setup and run the model for mint 1
+coda.bern.mint1 <- generate.jags.bernouilli.samples(data.dt[mintid == 1]);
+coda.bern.mint2 <- generate.jags.bernouilli.samples(data.dt[mintid == 2]);
+coda.bern.mint3 <- generate.jags.bernouilli.samples(data.dt[mintid == 3]);
