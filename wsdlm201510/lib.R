059ab06b955dc02cae3201f3535c95adb8248146
Mick Cooney <mickcooney@gmail.com>
2015-10-20 16:45:05 +0100

    Updated some work on the J1 + muJ2 work in Section 5

--- wsdlm201510/lib.R
+++ wsdlm201510/lib.R
@@ -56,3 +56,16 @@ plot.power.SINR.data <- function(data) {
 
     grid.arrange(power.plot, sinr.plot, nrow = 2);
 }
+
+
+calculate.xmu <- function(mu, A, y) {
+    AtA <- t(A) %*% A;
+
+    N <- dim(AtA)[1];
+
+    invmat <- solve(AtA + mu * diag(rep(1, N)));
+
+    xmu <- invmat %*% (t(A) %*% y)
+
+    return(xmu);
+}
