c3372ac4b18a3479608065a0000c6d64939a4a86
Mick Cooney <mickcooney@gmail.com>
2016-03-09 16:26:04 +0000

    Added a scratch_work.R file for the class and fixed some parameters for duration

--- wsbda201603/wsexercise.1.R
+++ wsbda201603/wsexercise.1.R
@@ -22,7 +22,7 @@ calculate.disease.test.probabilities(diseasedata.dt)
 false.alarm.seq <- seq(0, 0.1, by = 0.0005)
 
 infected.prob <- sapply(false.alarm.seq, function(iter.fa) {
-    data.dt <- generate.disease.test.data(n           = 1000000
+    data.dt <- generate.disease.test.data(n           = 100000
                                          ,prior.prob  = 0.001
                                          ,hit.rate    = 0.99
                                          ,false.alarm = iter.fa)
@@ -30,7 +30,7 @@ infected.prob <- sapply(false.alarm.seq, function(iter.fa) {
     calculate.disease.test.probabilities(data.dt)[infected == 1, prop]
 })
 
-qplot(false.alarm.seq, infected.prob, geom = 'line')
+qplot(false.alarm.seq, infected.prob, geom = 'line', ylim = c(0, 1))
 
 
 
@@ -49,7 +49,7 @@ twodisease.dt <- generate.disease.twotest.data(n             = 100000
 false.alarm.seq <- seq(0, 0.1, by = 0.0005)
 
 twotest.infected.prob <- sapply(false.alarm.seq, function(iter.fa) {
-    data.dt <- generate.disease.twotest.data(n             = 1000000
+    data.dt <- generate.disease.twotest.data(n             = 100000
                                             ,prior.prob    = 0.001
                                             ,hit.rate.1    = 0.99
                                             ,false.alarm.1 = iter.fa)
@@ -57,4 +57,10 @@ twotest.infected.prob <- sapply(false.alarm.seq, function(iter.fa) {
     data.dt[test.1 == 1 & test.2 == 1][, .N, by = infected][, .(infected, prop = N / sum(N))][infected == 1, prop]
 })
 
-qplot(false.alarm.seq, twotest.infected.prob, geom = 'line')
+qplot(false.alarm.seq, twotest.infected.prob, geom = 'line', ylim =c(0, 1))
+
+
+qplot(false.alarm.seq, infected.prob, geom = 'line', ylim = c(0, 1)) +
+    geom_line(aes(y = twotest.infected.prob), colour = 'red') +
+    xlab("False Alarm Probability") +
+    ylab("Infected Probability")
