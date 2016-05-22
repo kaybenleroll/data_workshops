b6bf6ba661e53eeecd6292e7c4f292de97d06cef
Mick Cooney <mickcooney@gmail.com>
2016-05-22 22:15:37 +0100

    Added in the PPC checks - want to start on some summary graphics

--- wsbda201603/wsexercise.7.R
+++ wsbda201603/wsexercise.7.R
@@ -5,7 +5,7 @@ source("lib.R")
 ##### Exercise 7.1
 #####
 
-set.seed(42)
+set.seed(4242)
 
 prod_mu <- c(0.08, 0.11, 0.09, 0.11, 0.10)
 prod_K  <- c( 200,  250,  150,  200,  400)
@@ -38,19 +38,19 @@ chain_count  <- 8
 
 multipleprod_stanmodel <- stan_model(stan_file, verbose = TRUE)
 
-stan_data_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
-                     ,n_trials   = inputdata_dt[, length(unique(design_id))]
-                     ,trials     = inputdata_dt$trial_count
-                     ,success    = inputdata_dt$success
-                     ,cat_id     = inputdata_dt$prod_id
-                     ,priorShape = 1.1
-                     ,priorRate  = 0.011
-                     ,priorA     = 2
-                     ,priorB     = 2
-                      )
+standata_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
+                    ,n_trials   = inputdata_dt[, length(unique(design_id))]
+                    ,trials     = inputdata_dt$trial_count
+                    ,success    = inputdata_dt$success
+                    ,cat_id     = inputdata_dt$prod_id
+                    ,priorShape = 1.1
+                    ,priorRate  = 0.011
+                    ,priorA     = 2
+                    ,priorB     = 2
+                     )
 
 multipleprod_stanfit <- sampling(multipleprod_stanmodel
-                                ,data      = stan_data_lst
+                                ,data      = standata_lst
                                 ,algorithm = "NUTS"
                                 ,warmup    = warmup_count
                                 ,iter      = warmup_count + sample_count
@@ -58,10 +58,12 @@ multipleprod_stanfit <- sampling(multipleprod_stanmodel
                                 ,verbose   = TRUE
                                  )
 
-if(exists("save_rds_files")) saveRDS(multipleprod_stanfit, file = 'cached_data/multipleprod_stanfit.rds', compress = 'xz')
+if(exists("save_rds_files")) saveRDS(multipleprod_stanfit
+                                    ,file = 'cached_data/multipleprod_stanfit.rds'
+                                    ,compress = 'xz')
 
-monitor(multipleprod_stanfit, probs = c(0.1,0.5,0.9))
-traceplot(multipleprod_stanfit, pars = c("mu", "theta", "K")) + expand_limits(y = c(0,1))
+monitor(multipleprod_stanfit, probs = c(0.1,0.5,0.9), digits_summary = 3)
+traceplot(multipleprod_stanfit, pars = c("mu","K")) + expand_limits(y = c(0,0.15))
 
 
 
@@ -77,19 +79,19 @@ chain_count  <- 8
 
 multipleprod_lognorm_stanmodel <- stan_model(stan_file, verbose = TRUE)
 
-stan_data_lst <- list(n_cats    = inputdata_dt[, length(unique(prod_id))]
-                     ,n_trials  = inputdata_dt[, length(unique(design_id))]
-                     ,trials    = inputdata_dt$trial_count
-                     ,success   = inputdata_dt$success
-                     ,cat_id    = inputdata_dt$prod_id
-                     ,priorMean = 8
-                     ,priorSD   = 0.5
-                     ,priorA    = 2
-                     ,priorB    = 2
-                      )
+standata_lst <- list(n_cats    = inputdata_dt[, length(unique(prod_id))]
+                    ,n_trials  = inputdata_dt[, length(unique(design_id))]
+                    ,trials    = inputdata_dt$trial_count
+                    ,success   = inputdata_dt$success
+                    ,cat_id    = inputdata_dt$prod_id
+                    ,priorMean = 8
+                    ,priorSD   = 0.5
+                    ,priorA    = 2
+                    ,priorB    = 2
+                     )
 
 multipleprod_lognorm_stanfit <- sampling(multipleprod_lognorm_stanmodel
-                                        ,data      = stan_data_lst
+                                        ,data      = standata_lst
                                         ,algorithm = "NUTS"
                                         ,warmup    = warmup_count
                                         ,iter      = warmup_count + sample_count
@@ -97,15 +99,25 @@ multipleprod_lognorm_stanfit <- sampling(multipleprod_lognorm_stanmodel
                                         ,verbose   = TRUE
                                          )
 
-if(exists("save_rds_files")) saveRDS(multipleprod_lognorm_stanfit, file = 'cached_data/multipleprod_lognorm_stanfit.rds', compress = 'xz')
+if(exists("save_rds_files")) saveRDS(multipleprod_lognorm_stanfit
+                                    ,file = 'cached_data/multipleprod_lognorm_stanfit.rds'
+                                    ,compress = 'xz')
 
-monitor(multipleprod_lognorm_stanfit, probs = c(0.1,0.5,0.9))
-traceplot(multipleprod_lognorm_stanfit, pars = c("mu", "theta", "K")) + expand_limits(y = c(0,1))
+monitor(multipleprod_lognorm_stanfit, probs = c(0.1,0.5,0.9), digits_summary = 3)
+traceplot(multipleprod_lognorm_stanfit, pars = c("mu","K")) + expand_limits(y = c(0,0.15))
 
 
 #####
 ##### Exercise 7.5
 #####
+
+###
+### WARNING!!! I've had convergence issues with this next part and have not
+### been able to figure out why. It may be a good thing to explore in terms
+### of diagnostics etc.
+###
+### This could be just an artefact of some weird data we generated.
+###
 
 warmup_count <- 250
 sample_count <- 250
@@ -117,22 +129,26 @@ design_sd   <-  50
 toss_mean <- 1000
 toss_sd   <-   50
 
-multiple_more_dt <- generate_multiple_hier_trial_data(prod_mu, prod_K, design_mean, design_sd, toss_mean, toss_sd)
-inputdata_dt     <- multiple_more_dt[, .(prod_id, design_id, toss_count, success)]
+stan_file <- 'multipleprod_hierarchical.stan'
 
-stan_data_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
-                     ,n_trials   = inputdata_dt[, length(unique(design_id))]
-                     ,trials     = inputdata_dt$toss_count
-                     ,success    = inputdata_dt$success
-                     ,cat_id     = inputdata_dt$prod_id
-                     ,priorShape = 1.1
-                     ,priorRate  = 0.0011
-                     ,priorA     = 2
-                     ,priorB     = 2
-                      )
+multipleprod_stanmodel <- stan_model(stan_file, verbose = TRUE)
+
+multiple_more_dt <- generate_multiple_hier_trial_data(prod_mu, prod_K, design_mean, design_sd, toss_mean, toss_sd)
+inputdata_dt     <- multiple_more_dt[, .(prod_id = cat_id, design_id = trial_id, trial_count, success)]
+
+standata_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
+                    ,n_trials   = inputdata_dt[, length(unique(design_id))]
+                    ,trials     = inputdata_dt$trial_count
+                    ,success    = inputdata_dt$success
+                    ,cat_id     = inputdata_dt$prod_id
+                    ,priorShape = 1.1
+                    ,priorRate  = 0.0011
+                    ,priorA     = 2
+                    ,priorB     = 2
+                     )
 
 multipleprod_more_stanfit <- sampling(multipleprod_stanmodel
-                                     ,data      = stan_data_lst
+                                     ,data      = standata_lst
                                      ,algorithm = "NUTS"
                                      ,warmup    = warmup_count
                                      ,iter      = warmup_count + sample_count
@@ -140,10 +156,12 @@ multipleprod_more_stanfit <- sampling(multipleprod_stanmodel
                                      ,verbose   = TRUE
                                       )
 
-if(exists("save_rds_files")) saveRDS(multipleprod_more_stanfit, file = 'cached_data/multipleprod_more_stanfit.rds', compress = 'xz')
+if(exists("save_rds_files")) saveRDS(multipleprod_more_stanfit
+                                    ,file = 'cached_data/multipleprod_more_stanfit.rds'
+                                    ,compress = 'xz')
 
-monitor(multipleprod_more_stanfit, probs = c(0.1,0.5,0.9))
-traceplot(multipleprod_more_stanfit, pars = c("mu", "theta", "K")) + expand_limits(y = c(0,1))
+monitor(multipleprod_more_stanfit, probs = c(0.1,0.5,0.9), digits_summary = 3)
+traceplot(multipleprod_more_stanfit, pars = c("mu","K")) + expand_limits(y = c(0,0.15))
 
 
 #####
@@ -157,21 +175,21 @@ toss_mean <- 250
 toss_sd   <-  50
 
 multiple_lowtoss_dt <- generate_multiple_hier_trial_data(prod_mu, prod_K, design_mean, design_sd, toss_mean, toss_sd)
-inputdata_dt     <- multiple_lowtoss_dt[, .(prod_id, design_id, toss_count, success)]
+inputdata_dt     <- multiple_lowtoss_dt[, .(prod_id = cat_id, design_id = trial_id, trial_count, success)]
 
-stan_data_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
-                     ,n_trials   = inputdata_dt[, length(unique(design_id))]
-                     ,trials     = inputdata_dt$toss_count
-                     ,success    = inputdata_dt$success
-                     ,cat_id     = inputdata_dt$prod_id
-                     ,priorShape = 1.1
-                     ,priorRate  = 0.0011
-                     ,priorA     = 2
-                     ,priorB     = 2
-                      )
+standata_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
+                    ,n_trials   = inputdata_dt[, length(unique(design_id))]
+                    ,trials     = inputdata_dt$trial_count
+                    ,success    = inputdata_dt$success
+                    ,cat_id     = inputdata_dt$prod_id
+                    ,priorShape = 1.1
+                    ,priorRate  = 0.0011
+                    ,priorA     = 2
+                    ,priorB     = 2
+                     )
 
 multipleprod_lowtoss_stanfit <- sampling(multipleprod_stanmodel
-                                        ,data      = stan_data_lst
+                                        ,data      = standata_lst
                                         ,algorithm = "NUTS"
                                         ,warmup    = warmup_count
                                         ,iter      = warmup_count + sample_count
@@ -179,10 +197,12 @@ multipleprod_lowtoss_stanfit <- sampling(multipleprod_stanmodel
                                         ,verbose   = TRUE
                                          )
 
-if(exists("save_rds_files")) saveRDS(multipleprod_lowtoss_stanfit, file = 'cached_data/multipleprod_lowtoss_stanfit.rds', compress = 'xz')
+if(exists("save_rds_files")) saveRDS(multipleprod_lowtoss_stanfit
+                                    ,file = 'cached_data/multipleprod_lowtoss_stanfit.rds'
+                                    ,compress = 'xz')
 
-monitor(multipleprod_lowtoss_stanfit, probs = c(0.1,0.5,0.9))
-traceplot(multipleprod_lowtoss_stanfit, pars = c("mu", "theta", "K")) + expand_limits(y = c(0,1))
+monitor(multipleprod_lowtoss_stanfit, probs = c(0.1,0.5,0.9), digits_summary = 3)
+traceplot(multipleprod_lowtoss_stanfit, pars = c("mu","K")) + expand_limits(y = c(0,0.15))
 
 
 
@@ -193,21 +213,21 @@ toss.mean <- 1000
 toss.sd   <-   50
 
 multiple_lowdesign_dt <- generate_multiple_hier_trial_data(prod_mu, prod_K, design_mean, design_sd, toss_mean, toss_sd)
-inputdata_dt          <- multiple_lowdesign_dt[, .(prod_id, design_id, toss_count, success)]
+inputdata_dt          <- multiple_lowdesign_dt[, .(prod_id = cat_id, design_id = trial_id, trial_count, success)]
 
-stan_data_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
-                     ,n_trials   = inputdata_dt[, length(unique(design_id))]
-                     ,trials     = inputdata_dt$toss_count
-                     ,success    = inputdata_dt$success
-                     ,cat_id     = inputdata_dt$prod_id
-                     ,priorShape = 1.1
-                     ,priorRate  = 0.0011
-                     ,priorA     = 2
-                     ,priorB     = 2
-                      )
+standata_lst <- list(n_cats     = inputdata_dt[, length(unique(prod_id))]
+                    ,n_trials   = inputdata_dt[, length(unique(design_id))]
+                    ,trials     = inputdata_dt$trial_count
+                    ,success    = inputdata_dt$success
+                    ,cat_id     = inputdata_dt$prod_id
+                    ,priorShape = 1.1
+                    ,priorRate  = 0.0011
+                    ,priorA     = 2
+                    ,priorB     = 2
+                     )
 
 multipleprod_lowdesign_stanfit <- sampling(multipleprod_stanmodel
-                                          ,data      = stan_data_lst
+                                          ,data      = standata_lst
                                           ,algorithm = "NUTS"
                                           ,warmup    = warmup_count
                                           ,iter      = warmup_count + sample_count
@@ -215,7 +235,9 @@ multipleprod_lowdesign_stanfit <- sampling(multipleprod_stanmodel
                                           ,verbose   = TRUE
                                            )
 
-if(exists("save_rds_files")) saveRDS(multipleprod_lowdesign_stanfit, file = 'cached_data/multipleprod_lowdesign_stanfit.rds', compress = 'xz')
+if(exists("save_rds_files")) saveRDS(multipleprod_lowdesign_stanfit
+                                    ,file = 'cached_data/multipleprod_lowdesign_stanfit.rds'
+                                    ,compress = 'xz')
 
-monitor(multipleprod_lowdesign_stanfit, probs = c(0.1,0.5,0.9))
-traceplot(multipleprod_lowdesign_stanfit, pars = c("mu", "theta", "K")) + expand_limits(y = c(0,1))
+monitor(multipleprod_lowdesign_stanfit, probs = c(0.1,0.5,0.9), digits_summary = 3)
+traceplot(multipleprod_lowdesign_stanfit, pars = c("mu","K")) + expand_limits(y = c(0,0.15))
