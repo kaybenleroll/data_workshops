90e035343de5d20663401798cdb8ea51e3b559a4
Mick Cooney <mickcooney@gmail.com>
2018-04-06 23:01:39 +0100

    Adding to the generated network logic

---
+++ ws_network_201803/custom_functions.R
@@ -0,0 +1,40 @@
+
+run_network_model_assessment <- function(data_igraph, sample_func, n_iter = 1000) {
+    sim_data_tbl <- data_frame(sim_id = 1:n_iter) %>%
+        mutate(graph      = rerun(n_iter, sample_func())
+              ,trans      = map_dbl(graph, transitivity)
+              ,diam       = map_dbl(graph, diameter)
+              ,meandist   = map_dbl(graph, mean_distance)
+              ,max_degree = map_dbl(graph, function(x) x %>% igraph::degree() %>% max)
+        )
+
+    graph_vals_tbl <- data_frame(
+        parameter = c('trans','diam','meandist', 'max_degree')
+       ,graph_val = c(data_igraph %>% transitivity
+                     ,data_igraph %>% diameter
+                     ,data_igraph %>% mean_distance
+                     ,data_igraph %>% igraph::degree() %>% max
+        )
+    )
+
+    plot_data_tbl <- sim_data_tbl %>%
+        dplyr::select(-graph) %>%
+        gather('parameter','value', -sim_id)
+
+
+    assess_plot <- ggplot(plot_data_tbl) +
+        geom_histogram(aes(x = value), bins = 50) +
+        geom_vline(aes(xintercept = graph_val), colour = 'red', data = graph_vals_tbl) +
+        facet_wrap(~parameter, scales = 'free') +
+        scale_y_continuous(label = comma) +
+        xlab('Value') +
+        ylab('Count')
+
+
+    return(list(
+        sim_data_tbl   = sim_data_tbl
+       ,graph_vals_tbl = graph_vals_tbl
+       ,plot_data_tbl  = plot_data_tbl
+       ,assess_plot    = assess_plot
+    ))
+}
--- ws_network_201803/data_setup.R
+++ ws_network_201803/data_setup.R
@@ -3,6 +3,13 @@
 # Re-using some code from Sec 1 of the workshop to avoid repeating myself
 # in later worksheets.
 
+
+# Setup the Florentine marriage network
 data(flo, package = 'network')
 
 florence_igraph <- graph_from_adjacency_matrix(flo, mode = 'undirected')
+
+
+# Setup the lazega network
+
+lazega_igraph <- lazega %>% upgrade_graph()
