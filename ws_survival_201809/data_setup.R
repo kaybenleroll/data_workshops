7c6e87c7d8bd3ce65eea577af72946c6678eb8f4
Mick Cooney <mickcooney@gmail.com>
2018-09-22 21:23:54 +0100

    Added in the first two worksheet and working on the solutions

--- ws_survival_201809/data_setup.R
+++ ws_survival_201809/data_setup.R
@@ -1,7 +1,19 @@
 ### Data Setup Script
+library(tidyverse)
+library(asaur)
 
 # Re-using some code from Sec 1 of the workshop to avoid repeating myself
 # in later worksheets.
 
 
-# Setup the survival data
+# Setup the Telco churn data
+telco_churn_tbl <- read_csv('data/telcochurn.csv'
+                           ,col_types = cols()
+                            )
+
+# Setup the pharmacoSmoking data
+pharmaco_smoker_tbl <- asaur::pharmacoSmoking %>% as_data_frame()
+
+# Setup the prostate survival data
+prostate_surv_tbl <- asaur::prostateSurvival %>% as_data_frame()
+
