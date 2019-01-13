8477dc433a27b0146a9fe0c53585cfdd49728447
Mick Cooney <mickcooney@gmail.com>
2019-01-13 22:17:38 +0000

    Working on data manipulation but having searches issues

---
+++ ws_timeseries_201901/custom_functions.R
@@ -0,0 +1,20 @@
+retrieve_cran_download_data <- function(cran_pkgs, start_date, end_date) {
+
+    cran_data_tbl <- cran_downloads(cran_pkgs
+                                   ,from = start_date
+                                   ,to   = end_date) %>%
+        as_tibble()
+
+
+    cran_all_tbl  <- cran_downloads(NULL
+                                    ,from = start_date
+                                    ,to   = end_date) %>%
+        as_tibble() %>%
+        mutate(package = 'TOTAL')
+
+
+    cran_data_tbl <- bind_rows(cran_data_tbl, cran_all_tbl)
+
+
+    return(cran_data_tbl)
+}
