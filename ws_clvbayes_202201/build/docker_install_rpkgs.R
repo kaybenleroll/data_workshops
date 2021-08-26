29aac98e8a8fe17e892c4901b7fcbcf5bf85fe0c
Mick Cooney <mickcooney@gmail.com>
2021-08-26 11:32:32 +0100

    CLVBAYES: Working on the etau fit

--- ws_clvbayes_202201/build/docker_install_rpkgs.R
+++ ws_clvbayes_202201/build/docker_install_rpkgs.R
@@ -1,24 +1,6 @@
 remotes::install_github(
-  "r-lib/fs",
-  ref     = "4cc4b56c26b9d7f177a676fbb331133bb2584b86",
-  upgrade = "never"
-)
-
-remotes::install_github(
-  "business-science/tidyquant",
-  ref     = "a1d9b836b0f040cd86cd3b627ddea701b8a91bcf",
-  upgrade = "never"
-)
-
-remotes::install_github(
-  "stan-dev/posterior",
-  ref     = "02aaef5806f484e4e3b13aa1ff95124d590a097a",
-  upgrade = "never"
-)
-
-remotes::install_github(
   "paul-buerkner/brms",
-  ref     = "9b72e82941b941e084dc7a3e43e50460599e2639",
+  ref     = "v2.16.1",
   upgrade = "always"
 )
 
@@ -32,9 +14,9 @@ remotes::install_github(
 library(cmdstanr)
 
 cmdstan_flags <- list(
-  "CXX"        = "clang++-9",
+  "CXX"        = "clang++",
   "CXXFLAGS"   = "-Os -mtune=native -march=native  -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined",
-  "CXX14"      = "clang++-9",
+  "CXX14"      = "clang++",
   "CXX14FLAGS" = "-Os -mtune=native -march=native  -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined"
 )
 
