f29571f227f355e2e942a64dc3809bdc2074e137
Mick Cooney <mickcooney@gmail.com>
2022-01-04 12:46:59 +0000

    CLVBAYES: Working on the new logic but need to restart the material

--- ws_clvbayes_202201/lib_data_generation.R
+++ ws_clvbayes_202201/lib_data_generation.R
@@ -20,39 +20,8 @@ generate_customer_cohort_data <- function(n_customers, first_date, last_date) {
 }
 
 
-
-
-generate_pnbd_individual_transactions <- function(lifetime, tnx_rate, mx_nu, mx_p,
-                                                  first_date, final_date = final_date) {
-
-  obs_weeks <- difftime(final_date, first_date, units = "weeks") %>% as.numeric()
-
-  tnx_window <- min(obs_weeks, lifetime)
-
-  sim_count <- round(10 * (tnx_window * tnx_rate), 0) %>% max(10)
-
-  event_times <- rexp(sim_count, rate = tnx_rate)
-
-  tnx_intervals     <- cumsum(event_times)
-  use_tnx_intervals <- tnx_intervals[tnx_intervals < tnx_window]
-
-
-  first_tnx_dttm <- as.POSIXct(first_date) + runif(1, min = 0, max = 24 * 60 * 60 - 1)
-
-  event_dates <- first_tnx_dttm + (use_tnx_intervals * (7 * 24 * 60 * 60))
-
-  tnx_amounts <- rgamma(1 + length(event_dates), shape = mx_p, rate = mx_nu)
-
-  sim_tnx_tbl <- tibble(
-    tnx_timestamp = c(first_tnx_dttm, event_dates),
-    tnx_amount    = tnx_amounts %>% round(2)
-    )
-
-  return(sim_tnx_tbl)
-}
-
-
-generate_pnbd_customer_simulation_params <- function(customer_cohort_data_tbl, params_lst) {
+generate_pnbd_customer_simulation_params <- function(customer_cohort_data_tbl,
+                                                     params_lst) {
   mu_shape     <- params_lst$mu_shape
   mu_rate      <- params_lst$mu_rate
   lambda_shape <- params_lst$lambda_shape
@@ -69,13 +38,13 @@ generate_pnbd_customer_simulation_params <- function(customer_cohort_data_tbl, p
       customer_lambda = rgamma(n(), shape = lambda_shape, rate = lambda_rate),
       customer_nu     = rgamma(n(), shape = mx_q,         rate = mx_g),
       customer_p      = params_lst$mx_p
-    )
+      )
 
   return(customer_basic_parameters_tbl)
 }
 
 
-generate_pnbd_customer_transaction_data <- function(sim_params_tbl) {
+generate_pnbd_customer_transaction_data <- function(sim_params_tbl, final_tnx_date) {
   customer_transactions_tbl <- sim_params_tbl %>%
     mutate(
       sim_data = pmap(
@@ -85,9 +54,9 @@ generate_pnbd_customer_transaction_data <- function(sim_params_tbl) {
           mx_nu      = customer_nu,
           mx_p       = customer_p,
           first_date = first_tnx_date
-        ),
+          ),
         generate_pnbd_individual_transactions,
-        final_date = final_date_observed
+        final_date = final_tnx_date
         )
       ) %>%
     select(
@@ -96,6 +65,37 @@ generate_pnbd_customer_transaction_data <- function(sim_params_tbl) {
     unnest(sim_data)
 
   return(customer_transactions_tbl)
+}
+
+
+generate_pnbd_individual_transactions <- function(lifetime, tnx_rate, mx_nu, mx_p,
+                                                  first_date, final_date = final_date) {
+
+  obs_weeks <- difftime(final_date, first_date, units = "weeks") %>% as.numeric()
+
+  tnx_window <- min(obs_weeks, lifetime)
+
+  if(tnx_window < 0) warning("Invalid tnx_window value: should not be negative")
+
+  sim_count <- round(10 * (tnx_window * tnx_rate), 0) %>% max(10)
+
+  event_times <- rexp(sim_count, rate = tnx_rate)
+
+  tnx_intervals     <- cumsum(event_times)
+  use_tnx_intervals <- tnx_intervals[tnx_intervals < tnx_window]
+
+  first_tnx_dttm <- as.POSIXct(first_date) + runif(1, min = 0, max = 24 * 60 * 60 - 1)
+
+  event_dates <- first_tnx_dttm + (use_tnx_intervals * (7 * 24 * 60 * 60))
+
+  tnx_amounts <- rgamma(1 + length(event_dates), shape = mx_p, rate = mx_nu)
+
+  sim_tnx_tbl <- tibble(
+    tnx_timestamp = c(first_tnx_dttm, event_dates),
+    tnx_amount    = tnx_amounts %>% round(2)
+    )
+
+  return(sim_tnx_tbl)
 }
 
 
