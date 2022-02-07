7850d233c9a225b2608196d12b3331f8d35c0af4
Mick Cooney <mickcooney@gmail.com>
2022-02-07 17:53:19 +0000

    CLVBAYES: Finishing off the validation logic in the intial_tnx worksheet

--- ws_clvbayes_202201/lib_btyd.R
+++ ws_clvbayes_202201/lib_btyd.R
@@ -16,3 +16,24 @@ calculate_transaction_cbs_data <- function(tnx_data_tbl, last_date) {
   return(cbs_data_tbl)
 }
 
+
+validate_frequency_model <- function(freqmodel_stanfit, input_data_tbl) {
+
+  freqmodel_valid_tbl <- freqmodel_stanfit %>%
+    recover_types(input_data_tbl) %>%
+    spread_draws(lambda[customer_id]) %>%
+    ungroup() %>%
+    inner_join(input_data_tbl, by = "customer_id") %>%
+    transmute(
+      customer_id     = customer_id,
+      draw_id         = .draw,
+      customer_lambda = customer_lambda,
+      post_lambda     = lambda,
+      pred_count      = rpois(n(), lambda = lambda * tnx_window),
+      obs_count       = obs_count
+      ) %>%
+    replace_na(list(obs_count = 0))
+
+
+  return(freqmodel_valid_tbl)
+}
