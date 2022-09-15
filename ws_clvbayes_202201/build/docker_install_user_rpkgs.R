7be95412c89fe5b450c3e70c281786fdc1c908c6
Mick Cooney <mickcooney@gmail.com>
2022-09-15 10:33:18 +0000

    CLVBAYES: Adding some validation using p_alive to the pnbd worksheet

--- ws_clvbayes_202201/build/docker_install_user_rpkgs.R
+++ ws_clvbayes_202201/build/docker_install_user_rpkgs.R
@@ -15,9 +15,9 @@ library(cmdstanr)
 
 cmdstan_flags <- list(
   "CXX"        = "clang++",
-  "CXXFLAGS"   = "-Os -mtune=native -march=native  -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined",
+  "CXXFLAGS"   = "-Os -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined",
   "CXX14"      = "clang++",
-  "CXX14FLAGS" = "-Os -mtune=native -march=native  -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined"
+  "CXX14FLAGS" = "-Os -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined"
 )
 
 install_cmdstan(
