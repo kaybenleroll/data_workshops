5162a6758a71e4730de40fc1c30d407c78443227
Mick Cooney <mickcooney@gmail.com>
2022-10-13 10:28:46 +0100

    AXACLASS: Added wordcloud logic to the dataviz talk

--- talk_cirdas_axa_202209/build/docker_install_sys_rpkgs.R
+++ talk_cirdas_axa_202209/build/docker_install_sys_rpkgs.R
@@ -4,11 +4,3 @@ remotes::install_github(
   subdir  = "pkg",
   upgrade = "never"
 )
-
-
-remotes::install_github(
-  "gaospecial/wordcloud2",
-  ref     = "cd95cdb58e71a72970d24420f4d2487c067c7648",
-  upgrade = "never",
-)
-
