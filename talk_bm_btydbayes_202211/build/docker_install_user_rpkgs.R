e0af27db94428352d969187e7871f98c9b699bd4
Mick Cooney <mickcooney@gmail.com>
2022-10-17 10:21:45 +0000

    BTYDTALK: Adding the initial Dockerfile and Makefiles for the project - they need editing

---
+++ talk_bm_btydbayes_202211/build/docker_install_user_rpkgs.R
@@ -0,0 +1,36 @@
+remotes::install_github(
+  "paul-buerkner/brms",
+  ref     = "v2.17.0",
+  upgrade = "always"
+)
+
+remotes::install_github(
+  "stan-dev/cmdstanr",
+  ref     = "v0.5.3",
+  upgrade = "never"
+)
+
+
+remotes::install_github(
+  "rmcelreath/rethinking",
+  ref     = "v2.2.1",
+  upgrade = "never"
+)
+
+
+library(cmdstanr)
+
+cmdstan_flags <- list(
+  "CXX"        = "clang++",
+  "CXXFLAGS"   = "-Os -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined",
+  "CXX14"      = "clang++",
+  "CXX14FLAGS" = "-Os -Wno-unused-variable -Wno-unused-function  -Wno-unknown-pragmas -Wno-macro-redefined"
+)
+
+install_cmdstan(
+  cores       = parallel::detectCores(),
+  cpp_options = cmdstan_flags,
+  quiet       = FALSE,
+  overwrite   = FALSE,
+  version     = "2.30.1"
+)
