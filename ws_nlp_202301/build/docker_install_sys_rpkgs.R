702a29318be4dc29347281414afadf73e59a80de
Mick Cooney <mickcooney@gmail.com>
2023-01-03 16:34:09 +0000

    NLPSCRIPTS: Setting up the workshop and getting ready to scrape a list of film scripts

---
+++ ws_nlp_202301/build/docker_install_sys_rpkgs.R
@@ -0,0 +1,11 @@
+remotes::install_github(
+  "pommedeterresautee/fastrtext",
+  ref     = "b63c5de9a5168378e8e1abfc4a50be7292002bfb",
+  upgrade = "never"
+)
+
+install.packages("openNLPmodels.en",
+  dependencies = TRUE,
+  repos        = "http://datacube.wu.ac.at/"
+)
+
