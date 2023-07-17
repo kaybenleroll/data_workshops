8a5ad7e6d76174475e59f225692916e3f155e904
Mick Cooney <mickcooney@gmail.com>
2023-07-17 17:59:07 +0100

    WS-REPRORESEARCH - Adding in the files to set up the workshop

---
+++ ws_reproresearch_202307/lib_utils.R
@@ -0,0 +1,33 @@
+resolve_conflicts <- function(pkg_priority) {
+  get_index <- function(pkg_name) {
+    idx <- str_which(pkg_priority, pkg_name)
+
+    if(length(idx) == 0) {
+      idx <- 0L
+    }
+
+    return(idx)
+  }
+
+  conflict_lst <- conflict_scout()
+
+  for(func_name in names(conflict_lst)) {
+    pkg_index <- map_int(conflict_lst[[func_name]], get_index)
+
+    pkg_index <- pkg_index[pkg_index > 0]
+
+    if(length(pkg_index) == 0) {
+      pkg_use <- conflict_lst[[func_name]][1]
+    } else {
+      pkg_use <- pkg_index %>%
+        min() %>%
+        pkg_priority[.]
+
+    }
+
+    conflict_prefer(func_name, pkg_use)
+  }
+
+  return(conflict_lst)
+}
+
