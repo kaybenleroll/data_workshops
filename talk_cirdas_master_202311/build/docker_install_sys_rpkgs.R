8343cbef6739af9dd929d561b6bab224590839da
Mick Cooney <mickcooney@gmail.com>
2023-10-27 08:57:51 +0100

    CIRDAS-2023: Creating the slides for the talk

--- talk_cirdas_master_202311/build/docker_install_sys_rpkgs.R
+++ talk_cirdas_master_202311/build/docker_install_sys_rpkgs.R
@@ -1,6 +1,15 @@
 remotes::install_github(
+  "thomasp85/transformr",
+  ref     = "v0.1.3",
+  upgrade = "never"
+)
+
+
+remotes::install_github(
   "dutangc/CASdatasets",
   ref     = "cc69b33959a42f24b8aaaf732e7c3d623896eeea",
   subdir  = "pkg",
   upgrade = "never"
 )
+
+
